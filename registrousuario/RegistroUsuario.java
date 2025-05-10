191ef88754e9ac96df0c813454ef48e28333335f
CotzoM <mikeovidio11@gmail.com>
2025-05-10 12:35:54 -0600

    Agregado archivo RegistroUsuario.java

---
+++ registrousuario/RegistroUsuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mike.registrousuario;
+
+/**
+ *
+ * @author Mike Cotzojay
+ */
+public class RegistroUsuario {
+
+    public static void main(String[] args) {
+       Usuario Usuario = new Usuario();
+        Usuario.setVisible(true);
+        
+    }
+}
